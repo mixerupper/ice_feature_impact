4c282cbf6364f7338f13d6a74d73531251a37dc2
Anhthy Ngo <an3056@nyu.edu>
2021-06-08 11:46:49 -0400

    Finish comparator function

--- scripts/fi_comparators.py
+++ scripts/fi_comparators.py
@@ -1,22 +1,21 @@
-def build_comparator_table(X, model, fi_classes):
-	'''
-	Purpose: Build a table to compare our feature importance/impact metrics
-	@X: Dataset with features as column names
-	@model: Model we're analyzing
-	@fi_classes: List of feature importance/impact class objects
-		Each class should have standardized functions to fit and extract
-		the feature importance/impact table
-	'''
+def build_comparator_table(X, model, ice_object, fi_classes):
+    '''
+    Purpose: Build a table to compare our feature importance/impact metrics
+    @X: Dataset with features as column names
+    @model: Model we're analyzing
+    @ice: ICE object
+    @fi_classes: List of feature importance/impact class objects
+        Each class should have standardized functions to fit and extract
+        the feature importance/impact table
+    '''
 
-	output_table = pd.DataFrame({'feature':X.columns})
 
-	for fi in fi_classes:
-		# fi.fit(X, model)
-		# TODO: Merge each fi_class's feature importance table into the output table
-		# output_table = output_table\
-		# 	.merge(fi.fi_table, how = "left", on = "feature")
-		# TODO: Plot it
-		pass
+    output_table = ice_object.feature_impact_table()
 
-	return
-	# return output_table
+    for fi in fi_classes:
+        fi.fit(X, model)
+
+        output_table = output_table.\
+                merge(fi.fi_table(X), how = "left", on = "Feature")
+
+    return output_table
