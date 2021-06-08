d4eb703f675489f4cf4ef8e0fc35169f8153edd7
Anhthy Ngo <an3056@nyu.edu>
2021-06-07 23:28:37 -0400

    update shap class

--- scripts/shap_class.py
+++ scripts/shap_class.py
@@ -1,56 +1,69 @@
 class SHAP_FI():
-	def __init__(self, model_type, frac_sample = 0.9, seed_num = None, time = True, trace = False):
-		'''
-		Instantiates the ICE class
-		@param model_type : "binary" or "continuous" y-variable
-		@param frac_sample : Fraction of data set to sample for ICE df.
-		@param seed_num : Random seed for reproducibility.
-		@param trace : Turn on/off trace messages for debugging
-		@return ICE data (dataframe) with N observations.
-		@examples
-		ICE("binary", num_per_ob = 50, frac_sample = 0.5, seed_num = 420)
-		'''
-		self.model_type = model_type
-		self.frac_sample = frac_sample
-		self.seed_num = seed_num
-		self.trace = trace
-		self.time = time
 
-		# Initializations to raise exceptions
-		self.fit_all = False
+    def __init__(self, seed_num = None, time = True, trace = False, max_display = 999):
+        '''
+        Instantiates the SHAP_FI class.
+        @param seed_num : Random seed for reproducibility.
+        @param time: Set time functionality for runtime.
+        @param trace : Turn on/off trace messages for debugging.
+        @param max_display : Set max display of features.
+        @return ICE data (dataframe) with N observations.
+        @examples
+        SHAP_FI(420, time = True, trace = False, max_display = 10)
+        '''
+        self.seed_num = seed_num
+        self.trace = trace
+        self.time = time
+        self.max_display = max_display
 
-		self.fis = {}
+        self.shapley = []
 
-	def fit(self, X, model, lice = False):
-		'''
-		Creates all ICE datasets for each feature
-		@param X : Covariate matrix
-		@param model : Model to interpet
-		@param lice : Linearly spaced feature distribution instead of unique values
-		'''
-		pass
-		# shap.TreeExplainer(rf).shap_values(X)
+    def fit(self, X, model):
+        '''
+        Uses Shapley values to explain any machine learning model.
+        @param X : Covariate matrix
+        @param model : Model to interpet
+        '''
+        start = datetime.now()
+        shap_values = shap.Explainer(model).shap_values(X)
+        end = datetime.now()
 
-	def plot(self, save_path = None):
-		'''
-		Plot the SHAP values in a chart. Save to save_path
-		'''
+        if self.time:
+            print(f"Fit in {(end - start).total_seconds():.2f} seconds")
 
-		if save_path is not None:
-			fig.savefig(save_path,
-		            	bbox_inches = 'tight',
-		            	pad_inches = 1)
+        self.shapley.append(shap_values)
 
-	def fi_table(self):
-		fi_df = pd.DataFrame()
+        return
 
-		for feature in ice.ice_fis:
-			fi_df = fi_df\
-				.append(self.get_feature_impact(feature), ignore_index = True)
-		
-		for fi in fi_df.drop('Feature', axis = 1):
-			fi_df[f'Normalized {fi}'] = fi_df[fi]/fi_df[fi].sum()*100
+    def plot(self, save_path = None):
+        '''
+        Plot the SHAP values in a chart. Save to save_path
+        '''
 
-		fi_df = fi_df.fillna(0)
+        fig = shap.summary_plot(self.shapley[0][1], X, plot_type = "bar", max_display = self.max_display)
 
-		return fi_df
+
+        '''
+        if save_path is not None:
+            fig.savefig(save_path,
+                        bbox_inches = 'tight',
+                        pad_inches = 1)
+        '''
+
+
+    def fi_table(self, X):
+        '''
+        Return shapley value table
+        '''
+
+        features, vals = X.columns, np.abs(self.shapley[0][1]).mean(0)
+
+        fi_df = pd.DataFrame(list(zip(features, vals)), columns=['Feature','Shapley Value']).\
+               sort_values('Shapley Value', ascending = False).\
+                head(self.max_display).\
+                reset_index().\
+                drop(['index'], axis=1)
+
+
+        return fi_df
+    
